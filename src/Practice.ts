fd8ba453f72ede9e44d6e66f254ad3bff2a9086e
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-05-03 19:32:02 +0900

    기본 타입 연습

--- src/Practice.ts
+++ src/Practice.ts
@@ -1,4 +1,18 @@
-// string으로 선언 했기 때문에 string을 안넣을 경우 컴파일 에러 발생
-const message: string = "hello world";
-// const message: string = 2; => 에러
-console.log(message);
+let count = 0; // 숫자
+count += 1;
+// count = "문자열"; // 에
+
+const message: string = "hello world"; // 문자
+const done: boolean = true; // 불리언 값
+
+const numbers: number[] = [1, 2, 3]; // 숫자 배열
+
+const messages: string[] = ["hello", "world"]; // 문자열 배열
+// messages.push(1); // 숫자 넣으면 에러
+
+let mightBeUndefined: string | undefined = undefined; // string 혹은 undefined
+let nullableNumber: number | null = null; // number 혹은 null
+
+let color: "red" | "orange" | "yellow" = "red"; // red, orange, yellow 중 하나
+color = "yellow";
+// color = "green"; // 에러
