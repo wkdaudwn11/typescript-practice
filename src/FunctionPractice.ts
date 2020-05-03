f4293dfa2dfe715fc380273193d5d5c6ddd4d0c4
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-05-03 20:06:17 +0900

    Generic 연습 #2

--- src/FunctionPractice.ts
+++ src/FunctionPractice.ts
@@ -18,7 +18,25 @@ const merge = (a: any, b: any): any => {
   };
 };
 
+// merge 같은 경우엔 타입 유추가 모두 꺠진 것이므로 안좋은 방식임.
+// 저런 경우에 Generics를 사용하면 됨.
+const merge2 = <A, B>(a: A, b: B): A & B => {
+  return {
+    ...a,
+    ...b,
+  };
+};
+
+// Generic은 이렇게도 사용됨
+const wrap = <T>(param: T) => {
+  return {
+    param,
+  };
+};
+
 sum(1, 2);
 const total = sumArray([1, 2, 3, 4, 5]);
 returnNothing();
-const merged = merge({ foo: 1 }, { bar: 1 });
+const merged = merge({ foo: 1 }, { bar: 2 });
+const merged2 = merge2({ foo: 1 }, { bar: 2 });
+const wrapped = wrap(10);
