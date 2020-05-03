2e3ab421df75f20389557edde76cfbdf3d32c042
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-05-03 20:01:12 +0900

    Generics 타입 (any) 연습

--- src/FunctionPractice.ts
+++ src/FunctionPractice.ts
@@ -10,6 +10,15 @@ const returnNothing = (): void => {
   console.log("아무것도 반환하지 않는 함수");
 };
 
+// any 타입 (모두 허용)
+const merge = (a: any, b: any): any => {
+  return {
+    ...a,
+    ...b,
+  };
+};
+
 sum(1, 2);
 const total = sumArray([1, 2, 3, 4, 5]);
 returnNothing();
+const merged = merge({ foo: 1 }, { bar: 1 });
